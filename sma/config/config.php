33ac51ae57463fef5687a4fb89e8db27a6fd9dfc
Nelson Kiprono <nelsohnkiprono@gmail.com>
2022-08-15 06:23:36 +0000

    config.php edited online with Bitbucket

--- sma/config/config.php
+++ sma/config/config.php
@@ -19,8 +19,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 //$config['base_url'] = 'http://localhost/sanofipos/';
 //$config['base_url'] = 'https://powergas.techsavanna.technology/';
-//$config['base_url'] = 'https://powergas-home.techsavanna.technology/powergas-routes/';
-$config['base_url'] = 'https://powergas.techsavanna.technology/';
+$config['base_url'] = 'https://powergas-home.techsavanna.technology/powergas-test/';
+//$config['base_url'] = 'https://powergas.techsavanna.technology/';
 /*
 |--------------------------------------------------------------------------
 | Index File
